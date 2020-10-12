ce341d6b06668ff53354baa918d211e358a8ec01
Ricardo Ribeiro <rcribeiro@me.com>
2020-10-12 22:21:54 +0000

    Testing Gatsby with Strapi Headless CMS

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '3178e9ad20a10027f48d81b0451405c0'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '889bce1d-e702-49a8-8134-2f3eff32367c'
+};
